d7360d3c476cf4f3626a881f4061833d576b0d64
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-03-10 12:15:44 +0200

    started 6

---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IBookRepositiry.cs
@@ -0,0 +1,11 @@
+﻿namespace LNUBookShare.Application.Interfaces
+{
+    public interface IBookRepository
+    {
+        // Read (всі)
+        // Read (одна)
+        // Create
+        // Update
+        // Delete
+    }
+}
---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿
+namespace LNUBookShare.Application.Interfaces
+{
+    internal interface IUserRepository
+    {
+        // Read (всі)
+        // Read (одна)
+        // Create
+        // Update
+        // Delete
+    }
+}
