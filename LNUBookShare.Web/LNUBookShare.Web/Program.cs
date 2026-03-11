930c6de32c549c590ac142b600d9ffbd1119ea92
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-03-11 13:58:22 +0200

    secret connection string

--- LNUBookShare.Web/LNUBookShare.Web/Program.cs
+++ LNUBookShare.Web/LNUBookShare.Web/Program.cs
@@ -1,4 +1,15 @@
+// Це читає рядок з appsettings.json безпечно!
+using LNUBookShare.Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseNpgsql(connectionString);
+});
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
