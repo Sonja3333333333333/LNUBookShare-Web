64f6525cf0c6e8d16000486e684de6da5fb77345
barannapolci <apuhlij66@gmail.com>
2026-03-31 14:07:58 +0300

    реалізував бронь та чергу новий репозиторій та сервіс

---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IReservationRepository.cs
@@ -0,0 +1,11 @@
+﻿using LNUBookShare.Domain.Entities;
+
+namespace LNUBookShare.Application.Interfaces
+{
+    public interface IReservationRepository
+    {
+        Task AddAsync(ReservationQueue entry);
+        Task<bool> ExistsAsync(int bookId, int userId);
+        Task<int> GetPositionAsync(int bookId, int userId);
+    }
+}
---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IReservationService.cs
@@ -0,0 +1,12 @@
+﻿using LNUBookShare.Application.Common;
+
+namespace LNUBookShare.Application.Interfaces
+{
+    public interface IReservationService
+    {
+        Task<Result> ReserveBookAsync(int bookId, int userId);
+        Task<Result> JoinQueueAsync(int bookId, int userId);
+        Task<int> GetQueuePositionAsync(int bookId, int userId);
+        Task<bool> IsUserInQueueAsync(int bookId, int userId);
+    }
+}
