90a305c1399268744ce0be19e416de5a1eaf397e
Sonja3333333333333 <SOFIIA.BOHDANOVYCH@lnu.edu.ua>
2026-04-03 08:17:10 +0300

    added faculty service

---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IFacultyService.cs
@@ -0,0 +1,10 @@
+﻿using LNUBookShare.Application.Common;
+using LNUBookShare.Domain.Entities;
+
+namespace LNUBookShare.Application.Interfaces
+{
+    public interface IFacultyService
+    {
+        Task<Result<IEnumerable<Faculty>>> GetAllFacultiesAsync();
+    }
+}
---
+++ LNUBookShare.Web/LNUBookShare.Application/Services/FacultyService.cs
@@ -0,0 +1,23 @@
+﻿using LNUBookShare.Application.Common;
+using LNUBookShare.Application.Interfaces;
+using LNUBookShare.Domain.Entities;
+
+namespace LNUBookShare.Application.Services
+{
+    public class FacultyService : IFacultyService
+    {
+        private readonly IFacultyRepository _facultyRepository;
+
+        public FacultyService(IFacultyRepository facultyRepository)
+        {
+            _facultyRepository = facultyRepository;
+        }
+
+        public async Task<Result<IEnumerable<Faculty>>> GetAllFacultiesAsync()
+        {
+            var faculties = await _facultyRepository.GetAllAsync();
+
+            return Result<IEnumerable<Faculty>>.Success(faculties);
+        }
+    }
+}
