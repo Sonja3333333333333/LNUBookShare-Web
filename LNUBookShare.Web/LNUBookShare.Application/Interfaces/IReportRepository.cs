b9dca099a639ead1f842b0d4f24957399137dcc2
barannapolci <apuhlij66@gmail.com>
2026-04-28 14:36:25 +0300

    RepositoryForReporting

---
+++ LNUBookShare.Web/LNUBookShare.Application/Interfaces/IReportRepository.cs
@@ -0,0 +1,12 @@
+﻿using LNUBookShare.Domain.Entities;
+
+namespace LNUBookShare.Application.Interfaces
+{
+    public interface IReportRepository
+    {
+        Task AddAsync(Report report);
+        Task<bool> ExistsAsync(int senderId, int reportedUserId);
+        Task<IEnumerable<Report>> GetAllWithUsersAsync(); // Щоб адмін бачив імена
+        Task<Report?> GetByIdAsync(int id);
+    }
+}
